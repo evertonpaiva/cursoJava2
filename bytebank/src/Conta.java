aad8a9239b44b4a2e085026c512ab31cef618088
Everton Paiva <evertonpaiva@gmail.com>
2019-07-04 16:47:50 -0300

    Sintaxe mais enxuta

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -5,12 +5,12 @@ public class Conta {
     String titular;
     
     void deposita(double valor) {
-    	this.saldo = this.saldo + valor;
+    	this.saldo += valor;
     }
     
     public boolean saca(double valor) {
     	if(this.saldo >= valor) {
-    		this.saldo = this.saldo - valor;
+    		this.saldo -= valor;
     		return true;
     	} else {
     		return false;
