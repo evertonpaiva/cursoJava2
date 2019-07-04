4018d24ea1df32b7002528446d661b63fb99ca8d
Everton Paiva <evertonpaiva@gmail.com>
2019-07-04 16:46:40 -0300

    Testando métodos com retorno

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -3,4 +3,17 @@ public class Conta {
     int agencia = 42;
     int numero;
     String titular;
+    
+    void deposita(double valor) {
+    	this.saldo = this.saldo + valor;
+    }
+    
+    public boolean saca(double valor) {
+    	if(this.saldo >= valor) {
+    		this.saldo = this.saldo - valor;
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
 }
