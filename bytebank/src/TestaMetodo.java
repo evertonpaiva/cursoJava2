82a71bdff7aaa38f2b25be5a3c97cd7110ee23dd
Everton Paiva <evertonpaiva@gmail.com>
2019-07-05 09:21:58 -0300

    Implementando função para realizar transferência

--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -2,13 +2,28 @@
 public class TestaMetodo {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.saldo = 100;
-		conta.deposita(50);
-		System.out.println(conta.saldo);
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
 		
-		boolean conseguiuRetirar = conta.saca(20);
-		System.out.println(conta.saldo);
+		boolean conseguiuRetirar = contaDoPaulo.saca(20);
+		System.out.println(contaDoPaulo.saldo);
 		System.out.println(conseguiuRetirar);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		boolean sucessoTransferencia;
+		
+		sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+		
+		if(sucessoTransferencia) {
+			System.out.println("Transferência com sucesso");
+		} else {
+			System.out.println("Faltou dinheiro");
+		}
+		System.out.println(contaDaMarcela.saldo);
+		System.out.println(contaDoPaulo.saldo);
 	}
 }
