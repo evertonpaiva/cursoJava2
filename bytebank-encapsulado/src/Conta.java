2da8cbbdb4ea4bde3da55f3689a4e5f992d4ce90
Everton Paiva <evertonpaiva@gmail.com>
2019-07-05 10:28:23 -0300

    Testando Getters e Setters de referência

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -35,7 +35,23 @@ public class Conta {
     	return this.numero;
     }
     
-    public void setNumero(int novoNUmero) {
-    	this.numero = novoNUmero;
+    public void setNumero(int numero) {
+    	this.numero = numero;
     }
+    
+    public int getAgencia() {
+		return this.agencia;
+	}
+    
+    public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+    
+    public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+    
+    public Cliente getTitular() {
+		return titular;
+	}
 }
